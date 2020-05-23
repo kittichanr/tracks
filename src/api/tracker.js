147d3c2840d0933ee0205da99c29662f25864cbd
Petch <petchkubb@live.com>
2020-05-23 16:35:37 +0700

    add ngrok

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://cd3380c1.ngrok.io',
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,25 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({email, password}) => {
+    // make api request to signup with that email and password
+    //if we sign up, modify our state, and say that we are authenticated.
+    //if signing up fail, we probably nedd to reflect error message
+    //somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({email, password}) => {};
+};
+
+const signout = (dispatch) => {
+  return ({email, password}) => {};
+};
+
 export const {Provider, Context} = createDataContext(
   authReducer,
-  {},
+  {signin, signout, signup},
   {isSignedIn: false},
 );
