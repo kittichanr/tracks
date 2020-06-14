41cb3f7a1dfbe83baa74bea781956d3214944b2d
Petch <petchkubb@live.com>
2020-06-14 15:06:30 +0700

    add styles

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -15,13 +15,16 @@ import {Provider as AuthProvider, Context} from './src/context/AuthContext';
 import {Provider as LocationProvider} from './src/context/LocationContext';
 import {Provider as TrackProvider} from './src/context/TrackContext';
 
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function TrackList() {
   return (
-    <Stack.Navigator initialRouteName="TrackList">
-      <Stack.Screen name="TrackList" component={TrackListScreen} />
+    <Stack.Navigator initialRouteName="Tracks">
+      <Stack.Screen name="Tracks" component={TrackListScreen} />
       <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
     </Stack.Navigator>
   );
@@ -43,9 +46,33 @@ const App = () => {
         </Stack.Navigator>
       ) : (
         <Tab.Navigator initialRouteName="TrackList">
-          <Tab.Screen name="TrackList" component={TrackList} />
-          <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
-          <Tab.Screen name="Account" component={AccountScreen} />
+          <Tab.Screen
+            name="Tracks"
+            component={TrackList}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <FontAwesome name="th-list" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Add Track"
+            component={TrackCreateScreen}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <MaterialIcons name="add" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Account"
+            component={AccountScreen}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <FontAwesome name="gear" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       )}
     </NavigationContainer>
